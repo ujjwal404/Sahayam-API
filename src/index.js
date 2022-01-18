89401e21941c91ff9c06b9052a6f81ef2b471c17
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2022-01-19 01:42:08 +0530

    added cors origin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Dependencies
 require('dotenv').config();
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const helmet = require('helmet');
 const cors = require('cors');
 
@@ -11,7 +11,7 @@ const DB = process.env.ATLAS_DB;
 
 // Modules
 const DatabaseConnect = require('./DatabaseConnect');
-const schema = require("./GQLModel");
+const schema = require('./GQLModel');
 const Utils = require('./Utils');
 
 // Models
@@ -21,34 +21,33 @@ const ValidationModel = require('./ValidationModels');
 // Express App init
 const app = express();
 app.use(
-    helmet({
-      contentSecurityPolicy:
-      process.env.NODE_ENV === 'production' ? undefined : false,
-    }),
+	helmet({
+		contentSecurityPolicy: process.env.NODE_ENV === 'production' ? undefined : false
+	})
 );
-app.use(cors());
+app.use(cors({ origin: 'https://localhost:3000' }));
 DatabaseConnect.connect(DB);
 
 // Apollo Server Config
 const server = new ApolloServer({
-  schema,
-  context: ({req}) => {
-    const token = req.headers.authorization;
-    const contextID = Utils.getUser(token);
-    // console.log(user);
-    return {DBModel, ValidationModel, contextID};
-  },
+	schema,
+	context: ({ req }) => {
+		const token = req.headers.authorization;
+		const contextID = Utils.getUser(token);
+		// console.log(user);
+		return { DBModel, ValidationModel, contextID };
+	}
 });
-server.applyMiddleware({app, path: '/api'});
+server.applyMiddleware({ app, path: '/api' });
 
 // Express API if needed
 app.get('/', (req, res) => {
-  res.send('Please go to /api');
+	res.send('Please go to /api');
 });
 
 // Start the server
-app.listen({port}, () => {
-  console.log(`Graphql Express Server started on port ${port}!!!`);
-  console.log('CTRL+C to exit the logs and run ./stop.sh to stop the server');
-  console.log(`http://localhost:${port}`);
+app.listen({ port }, () => {
+	console.log(`Graphql Express Server started on port ${port}!!!`);
+	console.log('CTRL+C to exit the logs and run ./stop.sh to stop the server');
+	console.log(`http://localhost:${port}`);
 });
